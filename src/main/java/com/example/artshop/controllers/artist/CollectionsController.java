8fd29a547583b3d8ba971fccad0df20de8bb8964
ebeckerle <92605769+ebeckerle@users.noreply.github.com>
2023-03-08 15:05:13 -0600

    "3/8 - Models from drafts"

---
+++ src/main/java/com/example/artshop/controllers/artist/CollectionsController.java
@@ -0,0 +1,17 @@
+package com.example.artshop.controllers.artist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wardrobe/collections")
+public class CollectionsController {
+
+    @GetMapping
+    public String displayAllCollections(Model model){
+        model.addAttribute("title", "View All Collections");
+        return "artist/viewallcollections";
+    }
+}
---
+++ src/main/java/com/example/artshop/controllers/artist/ProfileController.java
@@ -0,0 +1,4 @@
+package com.example.artshop.controllers.artist;
+
+public class ProfileController {
+}
