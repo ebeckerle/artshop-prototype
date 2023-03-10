a8c04d6a09269ff3117593f8544742fa8fd3a517
ebeckerle <92605769+ebeckerle@users.noreply.github.com>
2023-03-10 07:50:30 -0600

    "3/10 - Models from drafts"

--- src/main/java/com/example/artshop/models/Artist.java
+++ src/main/java/com/example/artshop/models/Artist.java
@@ -11,5 +11,27 @@ public class Artist {
 
     private String name;
 
+
+    //@OnetoMany
     private List<Artwork> artwork;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Artwork> getArtwork() {
+        return artwork;
+    }
+
+    public void setArtwork(List<Artwork> artwork) {
+        this.artwork = artwork;
+    }
 }
