77f3e066ab8dad073075de898bc898d63c2202d2
ebeckerle <92605769+ebeckerle@users.noreply.github.com>
2023-03-14 09:27:01 -0500

    "3/14 - Setting up database"

--- src/main/java/com/example/artshop/controllers/artist/ArtistAdviceController.java
+++ src/main/java/com/example/artshop/controllers/artist/ArtistAdviceController.java
@@ -13,11 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.LongAdder;
-
 @ControllerAdvice("com.example.artshop.controllers.artist")
 public class ArtistAdviceController {
 
@@ -26,15 +21,6 @@ public class ArtistAdviceController {
     @Autowired
     protected CollectionRepository collectionRepository;
 
-//    @ModelAttribute("collections")
-//    public List<Collection> getCollections(Artist artist){
-//        return collectionRepository.findByArtist(artist);
-//    }
-
-//    @ModelAttribute("collections")
-//    public void addCollectionsToModel(Artist artist, Model model){
-//        model.addAttribute("collections", collectionRepository.findByArtist(artist));
-//    }
 
 //    @ModelAttribute("collections")
 //    public List<Collection> getCollections(){
@@ -45,7 +31,7 @@ public class ArtistAdviceController {
 //        return collections;
 //    }
 
-//    private ConcurrentHashMap<String, LongAdder> counterMap = new ConcurrentHashMap<>();
+
 //    @ModelAttribute("collections")
 //    public void getCollectionsByArtistInSession(HttpServletRequest request, Model model){
 ////        HttpSession session = request.getSession();
@@ -57,8 +43,7 @@ public class ArtistAdviceController {
 //    }
 
     @ModelAttribute("collections")
-    public void getCollectionsByArtistInSession(@PathVariable Integer artistId, Model model){
-
+    public void getCollectionsByArtistOnPath(@PathVariable Integer artistId, Model model){
         Artist artist = artistRepository.findById(artistId).get();
         model.addAttribute("collections", collectionRepository.findByArtist(artist));
     }
--- src/main/java/com/example/artshop/controllers/artist/CollectionsController.java
+++ src/main/java/com/example/artshop/controllers/artist/CollectionsController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/wardrobe/collections")
-//@RequestMapping("/wardrobe/{artistId}/collections")
+//@RequestMapping("/wardrobe/collections")
+@RequestMapping("/wardrobe/{artistId}/collections")
 public class CollectionsController {
 
     @GetMapping
