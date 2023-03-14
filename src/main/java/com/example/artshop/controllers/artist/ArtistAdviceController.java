e998794f2dd3d5aad75fd683de3eec14ca4f0991
ebeckerle <92605769+ebeckerle@users.noreply.github.com>
2023-03-14 09:03:01 -0500

    "3/14 - Setting up database"

--- src/main/java/com/example/artshop/controllers/artist/ArtistAdviceController.java
+++ src/main/java/com/example/artshop/controllers/artist/ArtistAdviceController.java
@@ -1,18 +1,28 @@
 package com.example.artshop.controllers.artist;
 
+import com.example.artshop.data.ArtistRepository;
 import com.example.artshop.data.CollectionRepository;
 import com.example.artshop.models.entity.Artist;
 import com.example.artshop.models.entity.Collection;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
 
 @ControllerAdvice("com.example.artshop.controllers.artist")
 public class ArtistAdviceController {
 
+    @Autowired
+    private ArtistRepository artistRepository;
     @Autowired
     protected CollectionRepository collectionRepository;
 
@@ -20,6 +30,38 @@ public class ArtistAdviceController {
 //    public List<Collection> getCollections(Artist artist){
 //        return collectionRepository.findByArtist(artist);
 //    }
+
+//    @ModelAttribute("collections")
+//    public void addCollectionsToModel(Artist artist, Model model){
+//        model.addAttribute("collections", collectionRepository.findByArtist(artist));
+//    }
+
+//    @ModelAttribute("collections")
+//    public List<Collection> getCollections(){
+//        List collections = new ArrayList<>();
+//        Collection collection = new Collection();
+//        collection.setName("Earth");
+//        collections.add(collection);
+//        return collections;
+//    }
+
+//    private ConcurrentHashMap<String, LongAdder> counterMap = new ConcurrentHashMap<>();
+//    @ModelAttribute("collections")
+//    public void getCollectionsByArtistInSession(HttpServletRequest request, Model model){
+////        HttpSession session = request.getSession();
+////        Integer artistId = (Integer) session.getAttribute("user");
+//        Integer artistId = 4;
+//
+//        Artist artist = artistRepository.findById(artistId).get();
+//        model.addAttribute("collections", collectionRepository.findByArtist(artist));
+//    }
+
+    @ModelAttribute("collections")
+    public void getCollectionsByArtistInSession(@PathVariable Integer artistId, Model model){
+
+        Artist artist = artistRepository.findById(artistId).get();
+        model.addAttribute("collections", collectionRepository.findByArtist(artist));
+    }
 
 
 
--- src/main/java/com/example/artshop/controllers/artist/ArtistHomeController.java
+++ src/main/java/com/example/artshop/controllers/artist/ArtistHomeController.java
@@ -3,15 +3,20 @@ package com.example.artshop.controllers.artist;
 import com.example.artshop.data.ArtistRepository;
 import com.example.artshop.data.CollectionRepository;
 import com.example.artshop.models.entity.Artist;
+import com.example.artshop.models.entity.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/wardrobe")
-//@RequestMapping("/wardrobe/{artistId}")
+//@RequestMapping("/wardrobe")
+@RequestMapping("/wardrobe/{artistId}")
 public class ArtistHomeController {
 
     @Autowired
@@ -20,21 +25,34 @@ public class ArtistHomeController {
     @Autowired
     private CollectionRepository collectionRepository;
 
-    @GetMapping
-    //@GetMapping("/{artistId}")
-    public String displayArtistHomePage(Model model){
+//    @GetMapping
+//    //@GetMapping("/{artistId}")
+////    public String displayArtistHomePage(Model model){
 //        public String displayArtistHomePage(Model model, @PathVariable Integer artistId){
-//        artistRepository.findById(artistId);
-//        Artist artist = new Artist();
-//        collectionRepository.findByArtist(artist);
+////        artistRepository.findById(artistId);
+////        Artist artist = new Artist();
+////        collectionRepository.findByArtist(artist);
+//
+//
+//        model.addAttribute("title", "Artist Home page");
+//
+////        model.addAttribute("collections");
+////        model.addAttribute("collections", "collections");
+//
+//        Artist artist = artistRepository.findById(artistId).get();
+//        model.addAttribute("collections", collectionRepository.findByArtist(artist));
+//
+////        model.addAttribute("collections", ArrayList<Collection> collections);
+//        return "artist/home";
+//    }
 
+
+    @GetMapping
+    public String displayArtistHomePage(Model model, @PathVariable Integer artistId,
+                                        @ModelAttribute("collections") List<Collection> collections){
 
         model.addAttribute("title", "Artist Home page");
 
-        model.addAttribute("collections");
-        model.addAttribute("collections", "collections");
-
-//        model.addAttribute("collections", ArrayList<Collection> collections);
         return "artist/home";
     }
 }
